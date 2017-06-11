2fcde06125c80540926cae445f2ed25c175f56e4
Hilaly <hilalyamine@gmail.com>
2017-06-11 18:08:19 +0200

    licapy db manager

--- lwa/databases/databases.py
+++ lwa/databases/databases.py
@@ -29,8 +29,8 @@ _DELETE_DATA_BASE = "DROP DATABASE {0}"
 _USE_DATA_BASE = "USE {0}"
 _SHOW_TABLE = "DESC {0}"
 _SHOW_ALL_TABLES = "show tables;"
-_ADD_COLLUMN =
-_ADD_VALUE
+_ADD_COLLUMN = ""
+_ADD_VALUE = ""
 
 class DBManager:
     # Mysql database manager
@@ -64,7 +64,7 @@ class DBManager:
             self.db = databases
 
     @property
-    def databases(self):
+    def _databases(self):
         if self.state == 2:
             raise _NotAuthorisedMethod('You are already connected to a database *p3')
         with self.connection.cursor() as cursor:
@@ -72,6 +72,17 @@ class DBManager:
             cursor.execute(sql)
             result = cursor.fetchall()
             return result
+
+    @property
+    def _dbtables(self):
+        if self.state == 1:
+            raise _NotAuthorisedMethod('Connected to a database before using')
+        with self.connection.cursor() as cursor:
+            sql = _SHOW_ALL_TABLES
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return result
+
 
     def create_database(self, dbname):
         if self.state == 2:
@@ -97,20 +108,123 @@ class DBManager:
             cursor.execute(sql)
             print('Deleted database : ', dbname)
 
-    def create_table(self, tablename, collumns):
+    def _table_columns(self, table):
         pass
 
-    def delete_table(self, tablename):
+    def _create_table(self, tablename, collumns):
         pass
 
-    def add_collumns(self, tablename, collumn):
+    def _delete_table(self, tablename):
         pass
 
-    def del_collumn(self, tablename, collumn):
+    def _add_columns(self, tablename, collumn):
         pass
 
-    def change_collumn(self, tablesname, collumn , new_collumns):
+    def _del_column(self, tablename, collumn):
         pass
+
+    def _change_column(self, tablesname, collumn , new_collumns):
+        pass
+
+
+LICAPY_DATABASES = ['Plantae',
+                    'Animalia',
+                    'Fungi',
+                    'Chromista',
+                    'Virus',
+                    'Bacteria',
+                    'Essential Oils',
+                    'Vegatal Oils']
+
+LICAPY_SUPPORT_DATABASES = ['LicapyDB']
+
+PLANTAE_DB = {'TreeData' : ('id', 'name', 'pname', 'level'),
+              'PlantsData' : ('id', 'name', 'pname', 'locations', 'caracteristiques')}
+
+ANIMALIA_DB = {}
+FUNGI_DB = {}
+CHROMISTA_DB = {}
+VIRUS_DB = {}
+BACTERIA_DB = {}
+ESSENTIAL_OILS_DB = {}
+VEGETAL_OILS_DB = {}
+
+LICAPY_DATABASES_EXPANDS = [PLANTAE_DB,
+                            ANIMALIA_DB,
+                            FUNGI_DB,
+                            CHROMISTA_DB,
+                            VIRUS_DB,
+                            BACTERIA_DB,
+                            ESSENTIAL_OILS_DB,
+                            VEGETAL_OILS_DB]
+
+
+def _compare_l1(ln, lp):
+    for i in ln:
+        if not i in lp:
+            return False
+    return True
+
+class _NotAuthorisedOperation(Exception):
+    pass
+
+class LicapyDBManager(DBManager):
+    # Licapy DataBase manager
+
+    def __init__(self, cnxargs=(), db=LICAPY_DATABASES, sdb=LICAPY_SUPPORT_DATABASES):
+        DBManager.__init__(connection=None, connection_args=cnxargs)
+        if self.state == 2:
+            raise _NotAuthorisedOperation('LicapyDBManager should connect to mysql')
+        self.db = db + sdb
+        if self._verify_db():
+            print('Licapy databases are all created in mysql!')
+        else:
+            print('Not all Licapy databases are in mysql use ._build_db')
+
+    def _verify_db(self):
+        return _compare_l1(self.db, self._databases)
+
+    def _build_db(self, ecrase=False):
+        dbs = self._databases
+        for database in self.db:
+            if database in dbs:
+                if ecrase:
+                    self.delete_database(database)
+                    self.create_database(database)
+            else:
+                self.create_database(database)
+
+    def _verify_db_hierarchy(self, db):
+        L = LICAPY_DATABASES
+        architecture = LICAPY_DATABASES_EXPANDS[L.index(db)]
+        if not architecture:
+            print('No architecture given')
+            return
+        self.use_database(db)
+        for table, content in architecture.items():
+            if _compare_l1(content, self._table_columns(table)):
+                return True
+            return False
+
+
+    def _build_db_architecture(self, db, ecrase=True):
+        L = LICAPY_DATABASES
+        architecture = LICAPY_DATABASES_EXPANDS[L.index(db)]
+        if not architecture:
+            print('No architecture given')
+            return
+        if not ecrase and self._verify_db_hierarchy(db):
+            print('Database already built correctly')
+            return
+        self.use_database(db)
+        for table, content in architecture.items():
+            if not table in self._dbtables:
+                self._create_table(table, content)
+            if _compare_l1(content, self._table_columns(table)) and ecrase:
+                self._delete_table(table)
+                self._create_table(table, content)
+
+
 
 def verify_database(dbname):
     return
