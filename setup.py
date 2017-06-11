63ecb0de3fbc6da6599b3f7bc937ec37ba883343
Hilaly <hilalyamine@gmail.com>
2017-06-11 23:15:37 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+
+    
+setup(
+    name='licapy-web-manager',
+    version='0.0.1',
+    description='lwa',
+    long_description=readme,
+    author='Imp Alpha lab',
+    author_email='hilalyamine@gmail.com',
+    url='',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
