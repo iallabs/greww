5c3cf8bcd4c3862562852214c0726b79e9e187d8
Hilaly <hilalyamine@gmail.com>
2017-06-12 17:03:17 +0200

    add

---
+++ lwa/databases/cmdline.py
@@ -0,0 +1,19 @@
+
+import sys
+from .databases import LicapyDBManager
+
+command = []
+
+for arg in sys.argv:
+    command += [arg]
+
+_option = ['--build', '--destroy', 'show' , 'fields', 'select', 'db']
+
+class __NotImplementedCommand(Exception):
+    pass
+
+def _treat_command_line(cmdargs):
+    # [python licapydb.py show databases]
+    if not cmd[0] in _options:
+        raise _NotImplementedCommand('Not implemented cmdline')
+    return 'yes'
