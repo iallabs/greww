99e19fdd4b99658568029e8475ed69fce7b13c9a
Hilaly <hilalyamine@gmail.com>
2017-06-12 17:54:33 +0200

    add

--- lwa/databases/cmdline.py
+++ lwa/databases/cmdline.py
@@ -7,16 +7,49 @@ command = []
 for arg in sys.argv:
     command += [arg]
 
-_option = ['--build', '--destroy', 'show' , 'fields', 'select', 'db']
+def build():
+    print('build')
+    pass
+
+def rebuild():
+    print('rebuild')
+    pass
+
+def destroy():
+    print('destroy')
+    pass
+
+def show_content():
+    print('show c')
+    pass
+
+def show_fields():
+    pass
+
+def show_tables():
+    pass
+
+def show_database():
+    pass
+
+_option = {'--build' : build,
+           '--rebuild' : rebuild,
+           '--destroy' : destroy,
+           '-s' : show_content,
+           '-t' : show_tables,
+           '-f' : show_fields,
+           '-d' : show_database}
+
 
 class __NotImplementedCommand(Exception):
     pass
 
 def _treat_command_line(cmdargs):
     # [python licapydb.py show databases]
-    if not cmd[0] in _options:
+    if not cmdargs[0] in list(_options.keys()):
         raise _NotImplementedCommand('Not implemented cmdline')
-    return 'yes'
+    cmd = list(cmdargs)
+    return _option[cmd[0]](*cmd[1::])
 
 if __name__ == '__main__':
     print(command)
