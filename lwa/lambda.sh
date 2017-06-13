091fe127ba43ddc2df34a254f503035dfb99e1a6
Hilaly <hilalyamine@gmail.com>
2017-06-14 00:15:38 +0200

    sh

--- lwa/lambda.sh
+++ lwa/lambda.sh
@@ -1,6 +1,31 @@
 
-licapydb () {
-    python /home/ec2-user/projects/licapy-web-api/lwa/databases/cmdline.py {0}
+dbmanager () {
+    python /home/ec2-user/projects/licapy-web-api/lwa/databases/_dbmanager.py $0
 }
 
-alias licapydb='ldp'
+api () {
+    python /home/ec2-user/projects/licapy-web-api/lwa/databases/_api.py $0
+}
+
+getla () {
+    cd /home/ec2-user/projects
+    rm -rf licapy-api
+    git clone https://github.com/ImperialAlphaLab/licapy-api
+}
+
+buildla () {
+    cd /home/ec2-user/projects/licapy-api
+    python3 setup.py install
+}
+
+newla () {
+    getla
+    buildla
+}
+
+testla() {
+    cd /home/ec2-user/projects/licapy-api
+    python3 setup.py test
+}
+
+alias dbmanager='dbm'
