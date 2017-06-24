01fd6d9684fc47046883762ee3b4066c26a4bc82
Hilaly <hilalyamine@gmail.com>
2017-06-24 22:25:39 +0200

    add

--- mysql_utils.sh
+++ mysql_utils.sh
@@ -1,20 +1,18 @@
 mysql_info () {
-
+    python3 $1
 }
 
 mysql_acces () {
-
+    python3 $1
 
 }
 
-mysql_logs () {
-
-
+mysql_data () {
+    python3 $1
 }
 
 jmysql_utils () {
-
-
+    python3 $1
 }
 
 
