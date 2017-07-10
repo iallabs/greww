4f2e2df704a418453a5806b02ae08911ae324871
A.H <hilalyamine@gmail.com>
2017-07-10 19:17:55 +0200

    Update __init__.py

--- greww/__init__.py
+++ greww/__init__.py
@@ -2,16 +2,16 @@ from .utils.cboost import call_cpp_function
 
 GetSettings = call_cpp_function(GetSettings,
                                 path="settings.so",
-                                int_put="str";
+                                int_put="str",
                                 out_put="str")
 
 
 # General settings
 
-GeneralSettings = {
+GENERAL_SETTINGS = {
     WORKING_DIRECTORY = 1,
     OUTPUT_DIRECTORY = 1,
-    INPUT_DIRECTORY = 1,
+    INPUT_DIRECTORY = 1
 }
 
 JSON_SETTINGS =
@@ -20,7 +20,7 @@ MYSQL_SETTINGS = {
     HOST = 1,
     PORT = 1,
     USER = 1,
-    PASSWORD = 1,
+    PASSWORD = 1
 }
 
 SERVER_SETTINGS = {}
