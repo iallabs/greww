2bf05b4967b8f4fae112eae9b5dea8f2990de588
a-hilaly <hilalyamine@gmail.com>
2017-07-17 20:20:12 +0000

    axx

---
+++ cgreww_tests.sh
@@ -0,0 +1,41 @@
+# run Greww tests
+
+GrewTests () {
+
+}
+
+
+# run CGrew tests
+
+
+CGrewTest () {
+
+}
+
+
+# Combined tests
+
+CombinedGrewTests () {
+
+}
+
+# Run time compare
+
+# Grew
+RunTimeGrew () {
+
+}
+
+# CGrew
+RunTimeCGrew () {
+
+}
+
+#Compare
+
+ReportRunTimeGrewFeatures () {
+
+
+}
+
+$@
