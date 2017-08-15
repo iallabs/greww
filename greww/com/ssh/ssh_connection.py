4865750f86af16c34a8f15e105b4d9304fadfe5e
A-Hilaly <hilalyamine@gmail.com>
2017-08-15 18:58:40 +0200

    Bcc

--- greww/com/ssh/ssh_connection.py
+++ greww/com/ssh/ssh_connection.py
@@ -2,16 +2,12 @@ from greww.data.basics import stdir
 from greww.utils.exceptions import RejectedConnection, FileNotFound
 import paramiko
 
-
-# allow_agent, look_forkeys
-# paramiko
 def ssh_connection(hostname=None, port=None, username=None, password=None):
     if hostname is None or port is None:
         return -1
     client = paramiko.Transport((hostname, port))
     client.connect(username=username, password=password)
     return client
-
 
 def ssh_session(ssh_connection=None,
                 hostname=None,
@@ -30,7 +26,6 @@ def ssh_session(ssh_connection=None,
         session = session.open_channel(kind=kind)
         return session
 
-
 def private_rsa_key(location):
     return paramiko.RSAkey.from_private_key_file(location)
 
@@ -48,8 +43,6 @@ def ssh_connection_with_private_key(server=None,
 
 def send_ssh_command(ssh_connection=None):
     pass
-
-
 
 class sshConnection(object):
     def __init__(self):
