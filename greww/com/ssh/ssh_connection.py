be9c8908251d3479d2bf11a040c0342a72c2aba9
A-Hilaly <hilalyamine@gmail.com>
2017-08-15 18:54:44 +0200

    add

--- greww/com/ssh/ssh_connection.py
+++ greww/com/ssh/ssh_connection.py
@@ -13,7 +13,12 @@ def ssh_connection(hostname=None, port=None, username=None, password=None):
     return client
 
 
-def ssh_session(ssh_connection=None, hostname=None, username=None, password=None, kind=None):
+def ssh_session(ssh_connection=None,
+                hostname=None,
+                username=None,
+                password=None,
+                kind=None):
+
     if ssh_connection:
         session = ssh_connection.open_channel(kind=kind)
         return session
@@ -30,11 +35,16 @@ def private_rsa_key(location):
     return paramiko.RSAkey.from_private_key_file(location)
 
 #with key
-def ssh_connection_with_private_key(server=None, user=None, location=None, pkey=None, allow_agent=False, look_for_keys=True):
+def ssh_connection_with_private_key(server=None,
+                                    user=None,
+                                    location=None,
+                                    pkey=None,
+                                    allow_agent=False,
+                                    look_for_keys=True):
+
     pkey = pkey or private_rsa_jey(location)
     c = paramiko.SSHCLient()
     #FIXME: pls
-
 
 def send_ssh_command(ssh_connection=None):
     pass
