a50d69acba2f2044262990871fef142686996db1
A-Hilaly <hilalyamine@gmail.com>
2017-08-16 18:54:00 +0200

    Fix table test

--- greww/data/tests/tests_mysql.py
+++ greww/data/tests/tests_mysql.py
@@ -42,13 +42,13 @@ def test_connection_cursors():
     cursor.execute("SHOW DATABASES")
     _databases = [i for i in cursor]
     assert _databases != []
-    assert len(_databases) > 4
+    assert len(_databases) = 4
 
 def test_mysql_database_manipulation():
     db = databases()
     assert db
     print("hi", db)
-    assert len(db) > 4
+    assert len(db) = 4
     assert "sys" in db
     make_database("zilean_tests_0")
     db = databases()
@@ -56,8 +56,6 @@ def test_mysql_database_manipulation():
     remove_database("zilean_tests_0")
     db = databases()
     assert not "zilean_tests_0" in db
-
-test_mysql_database_manipulation()
 
 def test_mysql_tables_manipulation():
     make_database("zilean_tests_0")
