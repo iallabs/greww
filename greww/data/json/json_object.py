89d469e0398acdde39bc2ea7b6651b795297c4e4
A-Hilaly <hilalyamine@gmail.com>
2017-08-17 20:48:48 +0200

    json touches and travis

--- greww/data/json/json_object.py
+++ greww/data/json/json_object.py
@@ -1,11 +1,7 @@
-from greww.data.basics import (mkfile_with_content,
-			       			   stdir,
-                               file_content)
-import json
-
-
 def jsonize_kwargs(*args, **kwargs):
-    #A = list(args)
+    """
+	Return a Json object that contain all data stored at args and kwargs
+	"""
     keys = list(kwargs.keys())
     if not args:
         data = kwargs
@@ -16,20 +12,3 @@ def jsonize_kwargs(*args, **kwargs):
         for k in keys:
             data[k] = kwargs[k]
     return data
-
-def update_json_object(*args, **kwargs):
-    if kwargs["obj"] is None:
-        raise Exception()#TODO
-    obj=kwargs["obj"]
-    if args:
-        obj['args'] += list(args)
-    if kwargs:
-        for k in list(kwargs.keys()):
-            if k == "obj":
-                continue
-            obj[k] = kwargs[k]
-    return obj
-
-
-def get_json_object(file=None, byid=None, **kwarg):
-    pass
--- greww/data/json/json_utils.py
+++ greww/data/json/json_utils.py
@@ -3,121 +3,116 @@ from greww.data.basics import (mkfile_with_content,
                                stdir,
                                rmfile,
                                ckfile)
+from greww.utils.exceptions import (WTF,
+                                    LockedOption,
+                                    NotImplementedAlgo)
+from greww.shellenv import import_varenv
 
-def create_json_file(directory=DEFAULT_PATH, name=None, kind=None):
-    ct = '[]' if kind == list else "{}"
+
+GWP = import_varenv("GREWW_WORKING_PATH")
+
+def make_json(directory=GWP, name=None, kind=None, from_data=None):
+    """
+    Create json file at directory initiated as List Json or Dict Json
+    =================================================================
+    """
+    if kind:
+        ct = '[]' if kind == list else "{}"
+    if from_data:
+        ct = from_data
     mkfile_with_content(directory=directory,
-                         name=name,
-                         ext='json',
-                         content=ct)
+                        name=name,
+                        ext='json',
+                        content=ct)
 
-def append_json_object(directory=DEFAULT_PATH, name=None, obj=None):
-    if directory is None or name is None:
-        return
-        #TODO: Exception
+
+def feed_json(directory=GWP, name=None, obj=None):
+    if name is None:
+        raise ValueError("Name can't be None")
     stdir(directory)
-    try:
-        with open(name, 'r') as f:
-            data = json.load(f)
-            data.append(obj)
-        with open(name, 'w') as f:
-            json.dump(data, f)
-
-    except:
-        raise Exception()
-        #TODO: expetion
-
-#Upload for json dict
-def upload_json_object(directory=DEFAULT_PATH, name=None, obj=None):
-    if directory is None or name is None:
-        return
-        #TODO: Exception
-    stdir(directory)
-    try:
-        with open(name, 'r') as f:
-            data = json.load(f)
-            data.upload(obj)
-        with open(name, 'w') as f:
-            json.dump(data, f)
-
-    except:
-        raise Exception()
-        #TODO: expetion
-
-#XXX: this function actually works like all json files are list of json objects
-def count_json_objects(directory=DEFAULT_PATH, name=None):
-    if directory is None or name is None:
-        return
-        #TODO: Exception
-    stdir(directory)
-    try:
-        with open(name, 'r') as f:
-            data = json.load(f)
-            return len(data)
-    except:
-        raise Exception()
-        #TODO: expetion
-
-#XXX: this function actually works like all json files are list of json objects
-def del_json_object(directory=DEFAULT_PATH, name=None, **kwargs):
-    if directory is None or name is None:
-        return
-        #TODO: Exception
-    if not kwargs:
-        return
-        #
-    stdir(directory)
-    try:
-        with open(name, 'r') as f:
-            data = json.load(f)
-    except:
-        #TODO:
-        return
-    i = 0
-    res = []
-    for obj in data:
-        i += 1
-        for k in list(kwargs.keys()):
-            if not obj[k] == kwargs[k]:
-                res.append(obj)
-                continue
-    rmfile(directory, name)
-    with open(name, 'w') as f:
-        json.dump(res, f)
-
-
-def pythonize_json_file(directory=DEFAULT_PATH, name=None):
-    if directory is None or name is None:
-        return
-        #TODO: Exception
-    try:
-        with open(name, 'r') as f:
-            data = json.load(f)
-            return data
-    except:
-        #TODO:
-        return
-
-def get_json_object_from_file(directory=DEFAULT_PATH, name=None, **kwargs):
-    if directory is None or name is None:
-        return -1
-        #TODO: Exception
-    stdir(directory)
-    if not ckfile(directory, name):
-        return -1
-        #TODO: Exception
     with open(name, 'r') as f:
         data = json.load(f)
-    import collections
-    #FIXME: add possibility to get objects from one main json_object
+        if isinstance(data, list):
+            data.append(obj)
+        elif isinstance(data, dict):
+            data.upload(obj)
+        else:
+            raise WTF(file, data, obj)
+    with open(name, 'w') as f:
+        json.dump(data, f)
+
+def count_json(directory=GWP, name=None):
+    if name is None:
+        raise ValueError("Name can't be None")
+    stdir(directory)
+    with open(name, 'r') as f:
+        data = json.load(f)
+        return len(data)
+
+def unfeed_json(directory=GWP, name=None, **kwargs):
+    """
+    Remove all json objects under a json file that satify kwargs
+    logic
+    ==============================================================
+    """
+    if directory is None or name is None:
+        raise ValueError("Name can't be None")
+    if not kwargs:
+        raise LockedOption("Unauthorized Move")
+    stdir(directory)
+    with open(name, 'r') as f:
+        data = json.load(f)
+    if isinstance(data, list):
+        i = 0
+        res = []
+        for obj in data:
+            i += 1
+            for k in list(kwargs.keys()):
+                if not obj[k] == kwargs[k]:
+                    res.append(obj)
+                    continue
+        rmfile(directory, name)
+        with open(name, 'w') as f:
+            json.dump(res, f)
+
+    elif isinstance(data, list):
+        #TODO: add this part
+        raise NotImplementedAlgo(name)
+
+def read_json(directory=GWP, name=None):
+    """
+    Read a json file to return a Python object
+    ==============================================================
+    """
+    if name is None:
+        raise ValueError("Name can't be None")
+    with open(name, 'r') as f:
+        data = json.load(f)
+        return data
+
+def search_json(directory=GWP, name=None, **kwargs):
+    """
+    Try to find a list of objects that satisfy kwargs logic
+    ==============================================================
+    """
+    if name is None:
+        raise ValueError("Name can't be None")
+    stdir(directory)
+    with open(name, 'r') as f:
+        data = json.load(f)
     results = []
     w = True
-    for i in data:
-        for k in list(kwargs.keys()):
-            if i[k] != kwargs[k]:
-                w = False
-                break
-        if w:
-             results += [i]
-        w = True
-    return results
+    if isinstance(data, list):
+        for i in data:
+            for k in list(kwargs.keys()):
+                if i[k] != kwargs[k]:
+                    w = False
+                    break
+            if w:
+                 results += [i]
+            w = True
+        return results
+
+    elif isinstance(data, list):
+        #TODO: add this part
+        raise NotImplementedAlgo(name)
