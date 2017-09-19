c6bac8a62e886fb8851400291c4b8fc00a4997db
A.H <hilalyamine@gmail.com>
2017-09-19 16:07:47 +0200

    add

--- greww/data/json/json_utils.py
+++ greww/data/json/json_utils.py
@@ -117,3 +117,18 @@ def search_json(directory=GWP, name=None, **kwargs):
     elif isinstance(data, dict):
         #TODO: add this part
         raise NotImplementedAlgo(name)
+
+def jsonize_kwargs(*args, **kwargs):
+    """
+	Return a Json object that contain all data stored at args and kwargs
+	"""
+    keys = list(kwargs.keys())
+    if not args:
+        data = kwargs
+    else:
+        data = {
+             "args" : list(args),
+        }
+        for k in keys:
+            data[k] = kwargs[k]
+    return data
